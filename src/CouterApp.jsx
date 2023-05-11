50a519b52da3f74ce4c474906955e874f535574d
Emanuel <jramirezg3@miumg.edu.gt>
2023-05-11 11:49:46 -0600

    testeo de estilos y craecion de hooks

--- src/CouterApp.jsx
+++ src/CouterApp.jsx
@@ -1,18 +1,42 @@
+import { useState } from 'react'
 import PropTypes from 'prop-types'
 
 //recordar usar rafc para crear exportaciones rapidas
+
+
+
 export const CounterApp = ( {title, value} ) => {
+  const [ couter, setCounter ] = useState(value);
+
+
+  const handleAdd = () => { 
+
+    // setCounter ( couter + 1 );
+      setCounter( couter + 1 );
+  }
+  const handleSus = () => setCounter( couter - 1 );
   
+  const handleReset = () => setCounter( value );
+  
+
   // console.log( props );
   if (!title){
     throw new Error('El Titulo no existe');
   }
- 
+
     return (
     <>
         <h1>{ title }</h1>
         {/* <h1>{ JSON.stringify (newMessage) }</h1> */}
-        <h2>{value}</h2>
+        <h2> { couter } </h2>
+     
+      <button onClick={ handleAdd }> +1 </button>
+      
+      <button onClick={ handleSus }> -1 </button>
+     
+      <button onClick={ handleReset }> Restablecer </button>
+
+
     </>
     
   );
@@ -26,5 +50,5 @@ CounterApp.propTypes = {
 
 CounterApp.defaultProps = {
   title: 'CouterApp',
-  value: 10
+  value: 0
 }
